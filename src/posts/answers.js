cdf2a3b9827e2bc768c07e6f7f6d90fc1b964acf
Cathryn Lin <74870348+cathlyt@users.noreply.github.com>
2023-02-08 15:27:04 -0500

    Update backend saving to db

--- src/posts/answers.js
+++ src/posts/answers.js
@@ -1,8 +1,19 @@
 'use strict';
+const db = require('../database');
 
 module.exports = function (Posts) {
     Posts.markAsAnswer = async function (pid, uid) {
         console.log("we get here!!!!!!!!")
-        // return await db.sortedSetAdd(`uid:${uid}:markAsAnswer`, now, pid);
+        const test = await db.getObject(`pid:${pid}:answer`);
+        if (test == null){
+            db.sortedSetAdd(`pid:${pid}:answer`, Date.now(), 'true');
+        }else{
+            db.sortedSetRemove(`pid:${pid}:answer`, 'true');
+        }
+        
+        console.log(test);
+
+        return test;
     }
+
 }
