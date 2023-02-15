294ac7093032303dacb097ac3db5799ab494eb31
David Matovic <dmatovic@andrew.cmu.edu>
2023-02-15 14:54:46 -0500

    Initial routes and function set up

---
+++ src/topics/resolved.js
@@ -0,0 +1,36 @@
+"use strict";
+const { topic } = require("../middleware/assert");
+// var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+//     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+//     return new (P || (P = Promise))(function (resolve, reject) {
+//         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+//         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+//         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+//         step((generator = generator.apply(thisArg, _arguments || [])).next());
+//     });
+// };
+// Object.defineProperty(exports, "__esModule", { value: true });
+const topics = require("../topics");
+module.exports = function (Topics) {
+    Topics.markAsResolved = function (tid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            const resolved = yield topics.getTopicField(tid, ['resolved']);
+            if (resolved !== 'true') {
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                yield topics.setTopicField(tid, 'resolved', 'true');
+                console.log("*************************************************")
+                console.log(topics.getTopicField(tid, 'resolved'))
+            }
+            else {
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                yield topics.setTopicField(tid, 'resolved', 'false');
+                console.log("*************************************************")
+                console.log(topics.getTopicField(tid, 'resolved'))
+            }
+        });
+    };
+};
