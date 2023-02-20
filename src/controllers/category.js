3d1d8320fe7a1b2b18e082ed88f7f12496c19261
David Matovic <dmatovic@andrew.cmu.edu>
2023-02-20 15:12:51 -0500

    Added filtering for resolved topics to backend

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -94,6 +94,14 @@ categoryController.get = async function (req, res, next) {
         return next();
     }
 
+    categoryData.topics.forEach((topic) => {
+        topic.resolved = (topic.resolved === 'true');
+        topic.resolved_tag = topic.resolved ? 'true' : 'false';
+        console.log("THIS IS A TEST!!!")
+        console.log(topic.resolved === true)
+    });
+    console.log(categoryData.topics)
+
     categories.modifyTopicsByPrivilege(categoryData.topics, userPrivileges);
     categoryData.tagWhitelist = categories.filterTagWhitelist(categoryData.tagWhitelist, userPrivileges.isAdminOrMod);
 
