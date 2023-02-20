98deb93f352e7098e60a00e3bd23190fa9a832bc
Cathryn Lin <74870348+cathlyt@users.noreply.github.com>
2023-02-20 14:47:18 -0500

    Update field "is_resolved" to "resolved", update naming error

--- src/topics/resolved.ts
+++ src/topics/resolved.ts
@@ -9,15 +9,15 @@ module.exports = function (Topics:TopicObject) {
     Topics.markAsResolved = async function (tid:number) {
         // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const is_resolved:string = await topics.getTopicField(tid, ['is_resolved']) as string;
-        if (is_resolved !== 'true') {
+        const resolved:string = await topics.getTopicField(tid, ['resolved']) as string;
+        if (resolved !== 'true') {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await topics.setTopicField(tid, 'is_resolved', 'true');
+            await topics.setTopicField(tid, 'resolved', 'true');
         } else {
             // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            await topics.setTopicField(tid, 'is_resolved', 'false');
+            await topics.setTopicField(tid, 'resolved', 'false');
         }
     };
 };
